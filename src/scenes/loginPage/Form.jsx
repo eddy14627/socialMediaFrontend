457421568293b81a19bc2a9fef7dc766c82d4624
Adarsh Mishra <adjee2020@gmail.com>
2023-07-02 17:27:15 +0530

    updates

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -98,7 +98,7 @@ const Form = () => {
   const login = async (values, onSubmitProps) => {
     console.log("LOGGING IN");
     console.log(values);
-    const loggedInResponse = await fetch(`auth/login`, {
+    const loggedInResponse = await fetch(`/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
